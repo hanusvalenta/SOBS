abe7080cbc653b92a4809467f878d9a1744a725b
Hanuš Valenta <hanus.valenta@protonmail.com>
2023-03-25 13:57:13 +0100

    new version

--- Source/RamTest.rs
+++ Source/RamTest.rs
@@ -1,8 +1,15 @@
-fn main() 
+/*fn main() // Old version
 {
     loop
     {
        let mut a = Vec::new();
         a.push(1);
+    }
+}*/
+
+fn main() {
+    let mut v = Vec::new();
+    loop {
+        v.push(vec![0; 1000000]); // Allocate 1MB
     }
 }
